60678488ee34f43cfe5d23112e2e07f9a1559ef5
Alejandro Villa <alexandros17.sk@gmail.com>
2021-03-26 20:00:23 -0600

    fetch info for home view is ready

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import ErrorModal from '../../components/UIElements/ErrorModal/ErrorModal.component';
 import VideoListHome from '../../components/VideoListHome/VideoListHome.component';
-import { data } from '../../utils/youtube-videos-mock';
+import LoadingSpinner from '../../components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../utils/hooks/http-hook';
+//import { data } from '../../utils/youtube-videos-mock';
 
 const HomePage = () => {
-  return <VideoListHome data={data} />;
+  const [data, setData] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      const baseURL = process.env.REACT_APP_YOUTUBE_API_V3_SEARCH_URL;
+      const apikey = process.env.REACT_APP_GOOGLE_API_KEY;
+      const maxResults = 10;
+      const queryToSearch = 'goku vs vegeta majin buu';
+
+      try {
+        const responseData = await sendRequest(`${baseURL}?part=snippet&key=${apikey}&q=${queryToSearch}&maxResults=${maxResults}`)
+        console.log(responseData);
+        setData(responseData);
+      } catch (err) {
+        console.log(err);
+      };
+    };
+
+    fetchPlaces();
+  }, [sendRequest]);
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div>
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && data && (
+        <VideoListHome data={data} />
+      )}
+
+    </>
+  );
 };
 
 export default HomePage;
