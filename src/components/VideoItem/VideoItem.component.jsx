584be7d3ac07e7cbb80edaac1d423a699a04676f
Alejandro Villa <alexandros17.sk@gmail.com>
2021-03-28 20:25:38 -0600

    suggested videos are fetched correctly

---
+++ src/components/VideoItem/VideoItem.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const VideoItem = (props) => {
+  const history = useHistory();
+  const onClickHandler = () => {
+    history.push(`/videoDetails/${props.videoId}`);
+  };
+
+  return (
+    <div onClick={onClickHandler} aria-hidden="true">
+      <div className="videoItem__image">
+        <img src={`${props.image}`} alt={props.title} />
+      </div>
+      <div className="videoItem__info">
+        <h3>{props.title}</h3>
+      </div>
+    </div>
+  )
+};
+
+export default VideoItem;
--- src/pages/VideoDetails/VideoDetails.component.jsx
+++ src/pages/VideoDetails/VideoDetails.component.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
+import ListSuggestedVideos from '../../components/ListSuggestedVideos/ListSuggestedVideos.component';
 import ErrorModal from '../../components/UIElements/ErrorModal/ErrorModal.component';
 import LoadingSpinner from '../../components/UIElements/LoadingSpinner';
 import VideoPlayer from '../../components/VideoPlayer/VideoPlayer.component';
@@ -10,8 +11,20 @@ const VideoDetails = () => {
   const { videoId } = useParams();
 
   const [videoInfo, setVideoInfo] = useState();
-  // const [videosSuggested, setVideoSuggested] = useState();
-  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [videosSuggested, setVideoSuggested] = useState();
+  const {
+    isLoading: isLoadingFetchVideoInfo,
+    error: errorFetchVideoInfo,
+    sendRequest: sendRequestFetchVideoInfo,
+    clearError: clearErrorFetchVideoInfo,
+  } = useHttpClient();
+
+  const {
+    isLoading: isLoadingSuggestedVideos,
+    error: errorSuggestedVideos,
+    sendRequest: sendRequestSuggestedVideos,
+    clearError: clearErrorSuggestedVideos,
+  } = useHttpClient();
 
   useEffect(() => {
     const fetchVideoInfo = async (videoIdParam) => {
@@ -21,7 +34,7 @@ const VideoDetails = () => {
       const id = videoIdParam;
 
       try {
-        const responseData = await sendRequest(
+        const responseData = await sendRequestFetchVideoInfo(
           `${baseURL}?part=${part}&key=${apikey}&id=${id}`
         );
         setVideoInfo(responseData);
@@ -30,37 +43,41 @@ const VideoDetails = () => {
       }
     };
 
-    // const fecthVideosSuggested = async (videoIdParam) => {
-    //   const baseURL = process.env.REACT_APP_YOUTUBE_API_V3_VIDEOS_URL;
-    //   const apikey = process.env.REACT_APP_GOOGLE_API_KEY;
-    //   const part = 'snippet';
-    //   const relatedToVideoId = videoIdParam;
-    //   const maxResults = 10;
-    //   const type = 'video';
+    const fecthVideosSuggested = async (videoIdParam) => {
+      const baseURL = process.env.REACT_APP_YOUTUBE_API_V3_SEARCH_URL;
+      const apikey = process.env.REACT_APP_GOOGLE_API_KEY;
+      const part = 'snippet';
+      const relatedToVideoId = videoIdParam;
+      const maxResults = 10;
+      const type = 'video';
 
-    //   try {
-    //     const responseData = await sendRequest(
-    //       `${baseURL}?part=${part}&key=${apikey}&relatedToVideoId=${relatedToVideoId}&maxResults=${maxResults}&type=${type}`
-    //     );
-    //     setVideoSuggested(responseData);
-    //   } catch (err) {
-    //     console.log(err);
-    //   };
-    // };
+      try {
+        const responseData = await sendRequestSuggestedVideos(
+          `${baseURL}?part=${part}&key=${apikey}&relatedToVideoId=${relatedToVideoId}&maxResults=${maxResults}&type=${type}`
+        );
+        setVideoSuggested(responseData);
+      } catch (err) {
+        console.log(err);
+      };
+    };
 
     fetchVideoInfo(videoId);
-    // fecthVideosSuggested(videoId);
-  }, [sendRequest, videoId]);
+    fecthVideosSuggested(videoId);
+  }, [sendRequestFetchVideoInfo, sendRequestSuggestedVideos, videoId]);
 
   return (
     <>
-      <ErrorModal error={error} onClear={clearError} />
-      {isLoading && (
+      <ErrorModal error={errorFetchVideoInfo} onClear={clearErrorFetchVideoInfo} />
+      <ErrorModal error={errorSuggestedVideos} onClear={clearErrorSuggestedVideos} />
+      {isLoadingFetchVideoInfo && isLoadingSuggestedVideos && (
         <div>
           <LoadingSpinner />
         </div>
       )}
-      {!isLoading && videoInfo && <VideoPlayer videoInfo={videoInfo} />}
+      {!isLoadingFetchVideoInfo && videoInfo && <VideoPlayer videoInfo={videoInfo} />}
+      {!isLoadingSuggestedVideos && videosSuggested && (
+        <ListSuggestedVideos videosSuggested={videosSuggested.items} />
+      )}
     </>
   );
 };
